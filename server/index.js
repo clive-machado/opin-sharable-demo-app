ba2fdf67ff22e0a2e0f370386f816fb5e0ad7209
Clive Machado <clive.machado@raweng.com>
2020-12-10 22:27:48 +0530

    adds express server configuration to the code

---
+++ server/index.js
@@ -0,0 +1,19 @@
+var express    = require("express")
+
+var app        = express()
+const path = require("path");
+
+app.use("/opin-sharable-demo-app/", express.static(path.join(__dirname, "..", "build")));
+// app.use(express.static('public'));
+
+// app.use((req, res, next) => {
+//   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+// });
+
+app.get("/opin-demo-app", function (req, res, next) {
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+})
+
+app.listen(3001, function () {
+  console.log("app listening on port", 3001)
+})
